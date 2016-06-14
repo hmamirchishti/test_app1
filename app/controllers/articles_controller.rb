76a555ef13701ec3c2b5b5a27c68e8d3b3a1129c
Amir Sharif <hmamirchishti@gmail.com>
2016-06-15 00:39:24 +0500

    article edit, new, list has been added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,13 +2,28 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+        flash[:notice] = "Article is successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+  def index
+    @article = Article.all
+  end
   def create
     @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article is successfully Created."
       redirect_to article_path(@article)
     else
-      render 'new'
+      render 'edit'
     end
   end
   def show
