16f5d8b96c3f5df325caeffd2858248fd985d8dd
Amir Sharif <hmamirchishti@gmail.com>
2016-06-19 02:27:06 +0500

    added show for user and gravatar for profile picture

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,8 +26,10 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  def show
+    @user = User.find(params[:id])
+  end
   private
-
   def user_params
     params.require(:user).permit(:username, :email, :password)
 
